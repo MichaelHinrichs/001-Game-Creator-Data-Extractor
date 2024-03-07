7281984b2c6db3a0249a1cce8653c0580f07950c
NintenHero <37460517+MichaelHinrichs@users.noreply.github.com>
2024-03-06 23:59:51 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿//Written for games on 001 Game Creator. https://001gamecreator.com/
+
+using System.IO.Compression;
+
+BinaryReader br = new(File.OpenRead(args[0]));
+Directory.CreateDirectory(Path.GetDirectoryName(args[0]) + "\\" + Path.GetFileNameWithoutExtension(args[0]));
+br.BaseStream.Position += 8;
+int fileCount = br.ReadInt32();
+for (int i = 0; i < fileCount; i++)
+{
+    string name = new(br.ReadChars(br.ReadInt32()));
+    int sizeUncompressed = br.ReadInt32();
+    int size = br.ReadInt32();
+
+    using FileStream FS = File.Create(Path.GetDirectoryName(args[0]) + "//" + Path.GetFileNameWithoutExtension(args[0]) + "//" + name);
+    if (sizeUncompressed == -1)
+    {
+        BinaryWriter bw = new(FS);
+        bw.Write(br.ReadBytes(size));
+    }
+    else
+    {
+        br.ReadInt16();
+        using (var ds = new DeflateStream(new MemoryStream(br.ReadBytes(size - 2)), CompressionMode.Decompress))
+            ds.CopyTo(FS);
+    }
+
+    FS.Close();
+}
